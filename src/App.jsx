0a8aceda1583e9e760a530d462c4cea13d58bd56
bmdjtt <thinhndtse182967@fpt.edu.vn>
2025-01-14 09:08:10 +0700

    add Hosting pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import CustomerFrame from './pages/user-frame/CustomerFrame';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
+import Hosting from './pages/Hosting';
+import HostFrame from './pages/host-frame/HostFrame';
 
 function App() {
   const router = createBrowserRouter([
@@ -13,6 +15,13 @@ function App() {
       children: [
         {path: "", element: <HomePage/>},
         {path: "/about", element: <About/>},
+      ]
+    },
+    {
+      path: "/", 
+      element: <HostFrame/>,
+      children: [
+        { path: "hosting", element: <Hosting/> },
       ]
     }
   ])
---
+++ src/pages/Hosting.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react'
+const Hosting = () => {
+  return (
+    <div className="w-full h-screen  p-4 bg-gray-200">
+      <h1 className="text-4xl font-bold">Welcome back, Moc Moc</h1>
+        
+    </div>
+  );
+};
+
+export default Hosting
