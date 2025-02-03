3044a0033667e3aba423d4ec3b632a07834134c9
bmdjtt <thinhndtse182967@fpt.edu.vn>
2025-02-03 14:10:27 +0700

    update hosting

--- src/pages/Hosting.jsx
+++ src/pages/Hosting.jsx
@@ -4,7 +4,7 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 const Hosting = () => {
   const [activeTab, setActiveTab] = useState("");
-  const hostInfo= {
+  const hostInfo = {
     name: "Moc Moc",
     email: "mocmoc@gg.com",
     phone: "0123456789",
@@ -37,39 +37,128 @@ const Hosting = () => {
     ],
     reservation: {
       checkingOut: [
-        "Guest 1 - Checking Out",
-        "Guest 2 - Checking Out",
-        "Guest 3 - Checking Out",
-        "Guest 4 - Checking Out",
-        "Guest 5 - Checking Out",
+        {
+          name: "Guest 1",
+          location: "Hoang Hon Camping - Lakeview 1",
+          checkOutTime: "12:00 PM",
+          checkOutDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 2",
+          location: "Hoang Hon Camping - Lakeview 2",
+          checkOutTime: "12:00 PM",
+          checkOutDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 3",
+          location: "Hoang Hon Camping - Lakeview 3",
+          checkOutTime: "12:00 PM",
+          checkOutDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 4",
+          location: "Hoang Hon Camping - Lakeview 4",
+          checkOutTime: "12:00 PM",
+          checkOutDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 5",
+          location: "Hoang Hon Camping - Lakeview 5",
+          checkOutTime: "12:00 PM",
+          checkOutDate: "June 30, 2021",
+        },
       ],
       currentlyHosting: [
-        "Guest 1 - Currently Hosting",
-        "Guest 2 - Currently Hosting",
-        "Guest 3 - Currently Hosting",
-        "Guest 4 - Currently Hosting",
-        "Guest 5 - Currently Hosting",
+        {
+          name: "Guest 1",
+          location: "Hoang Hon Camping - Lakeview 1",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 2",
+          location: "Hoang Hon Camping - Lakeview 2",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 3",
+          location: "Hoang Hon Camping - Lakeview 3",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 4",
+          location: "Hoang Hon Camping - Lakeview 4",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 5",
+          location: "Hoang Hon Camping - Lakeview 5",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        }
       ],
       arrivingSoon: [
-        "Guest 1 - Arriving Soon",
-        "Guest 2 - Arriving Soon",
-        "Guest 3 - Arriving Soon",
-        "Guest 4 - Arriving Soon",
-        "Guest 5 - Arriving Soon",
+        {
+          name: "Guest 1",
+          location: "Hoang Hon Camping - Lakeview 1",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 2",
+          location: "Hoang Hon Camping - Lakeview 2",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 3",
+          location: "Hoang Hon Camping - Lakeview 3",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 4",
+          location: "Hoang Hon Camping - Lakeview 4",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 5",
+          location: "Hoang Hon Camping - Lakeview 5",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        }
       ],
       upcoming: [
-        "Guest 1 - Upcoming",
-        "Guest 2 - Upcoming",
-        "Guest 3 - Upcoming",
-        "Guest 4 - Upcoming",
-        "Guest 5 - Upcoming",
+        {
+          name: "Guest 1",
+          location: "Hoang Hon Camping - Lakeview 1",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 2",
+          location: "Hoang Hon Camping - Lakeview 2",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 3",
+          location: "Hoang Hon Camping - Lakeview 3",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        },
+        {
+          name: "Guest 4",
+          location: "Hoang Hon Camping - Lakeview 4",
+          checkInTime: "3:00 PM",
+          checkInDate: "June 30, 2021",
+        }
       ],
       pendingReview: [
-        "Review 1 - Pending",
-        "Review 2 - Pending",
-        "Review 3 - Pending",
-        "Review 4 - Pending",
-        "Review 5 - Pending",
       ],
     }
 
@@ -81,85 +170,40 @@ const Hosting = () => {
         <div>
           <h1 className="text-4xl font-bold py-8">Welcome back, {hostInfo.name}</h1>
         </div>
-        {/* <div className="grid grid-cols-1 md:grid-cols-4 gap-6 items-center justify-center">
-          <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-xl">
-            <div className="px-5 py-2">
-              <h6 className="mb-2 text-red-600 text-xl font-semibold">
-                Confirm important details
-              </h6>
-              <h6 className="text-red-600 text-l font-light">
-                Required to publish
-              </h6>
-              <p className="text-slate-600 leading-normal font-light">
-                Hoang Hon Camping - Lakeview 1
-              </p>
-            </div>
-            <div className="px-5 pb-4">
-              <button
-                className="text-black-500 underline hover:text-black-900 focus:outline-none disabled:text-black-700"
-                type="button"
-              >
-                Start
-              </button>
-
-            </div>
-          </div>
-          {/* <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-xl">
-            <div className="px-5 py-2">
-              <h6 className="mb-2 text-red-600 text-xl font-semibold">
-                Confirm important details
-              </h6>
-              <h6 className="text-red-600 text-l font-light">
-                Required to publish
-              </h6>
-              <p className="text-slate-600 leading-normal font-light">
-                Hoang Hon Camping - Lakeview 1
-              </p>
-            </div>
-            <div className="px-5 pb-4">
-              <button
-                className="text-black-500 underline hover:text-black-900 focus:outline-none disabled:text-black-700"
-                type="button"
-              >
-                Start
-              </button>
-            </div>
-          </div> 
-        </div> */}
-         <Swiper
-       modules={[Navigation]}
-       spaceBetween={30}
-       slidesPerView={4}
-       navigation
-       onSlideChange={() => console.log("slide change")}
-       onSwiper={(swiper) => console.log(swiper)}
-    >
-      {hostInfo.confirmInfo.map((info) => (
-      <SwiperSlide>
-        <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-xl">
-          <div className="px-5 py-2">
-            <h6 className="mb-2 text-red-600 text-xl font-semibold">
-              {info.title}
-            </h6>
-            <h6 className="text-red-600 text-l font-light">
-              {info.description}
-            </h6>
-            <p className="text-slate-600 leading-normal font-light">
-              {info.location}
-            </p>
-            <div className="px-5 pb-4">
-              <button
-                className="text-black-500 underline hover:text-black-900 focus:outline-none disabled:text-black-700"
-                type="button"
-              >
-                Start
-              </button>
-            </div>
-          </div>
+        <div className="mx-4">
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={30}
+            slidesPerView={4}
+            navigation
+          >
+            {hostInfo.confirmInfo.map((info) => (
+              <SwiperSlide>
+                <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-xl">
+                  <div className="px-5 py-2">
+                    <h6 className="mb-2 text-red-600 text-xl font-semibold">
+                      {info.title}
+                    </h6>
+                    <h6 className="text-red-600 text-l font-light">
+                      {info.description}
+                    </h6>
+                    <p className="text-slate-600 leading-normal font-light">
+                      {info.location}
+                    </p>
+                    <div className="px-5 pb-4">
+                      <button
+                        className="text-black-500 underline hover:text-black-900 focus:outline-none disabled:text-black-700"
+                        type="button"
+                      >
+                        Start
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-      </SwiperSlide>
-      ))}
-    </Swiper>
       </div>
       <div className="mx-auto px-30 max-w-7xl text-left">
         <div className="">
@@ -167,30 +211,26 @@ const Hosting = () => {
         </div>
         <div className="flex flex-row gap-6 items-start justify-start">
           <ul className="flex flex-row gap-6">
-            {[
-              { label: "Checking out (0)", value: "checkingOut" },
-              { label: "Currently hosting (0)", value: "currentlyHosting" },
-              { label: "Arriving soon (0)", value: "arrivingSoon" },
-              { label: "Upcoming (0)", value: "upcoming" },
-              { label: "Pending review (0)", value: "pendingReview" },
-            ].map((tab) => (
-              <li key={tab.value}>
+            {Object.keys(hostInfo.reservation).map((key) => (
+              <li key={key}>
                 <button
-                  className={`rounded-full border border-black py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg ${activeTab === tab.value
-                      ? "text-white bg-slate-800 border-slate-800"
-                      : "text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800"
+                  className={`rounded-full border border-black py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg ${activeTab === key
+                    ? "text-white bg-slate-800 border-slate-800"
+                    : "text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800"
                     }`}
                   type="button"
-                  onClick={() => setActiveTab(tab.value)}
+                  onClick={() => setActiveTab(key)}
                 >
-                  {tab.label}
+                  {key} ({hostInfo.reservation[key].length})
                 </button>
               </li>
             ))}
           </ul>
         </div>
-        <div className='bg-slate-100 rounded-lg w-auto h-60 my-4 text-center flex items-center justify-center align-middle'>
-          {activeTab === "checkingOut" && (
+        <div className='mt-4'>
+          {activeTab && hostInfo.reservation[activeTab].length === 0 && (
+            <div className='bg-slate-100 rounded-lg w-auto h-60 my-4 text-center flex items-center justify-center align-middle'>
+             {activeTab === "checkingOut" && (
             <p className=''>
               You don’t have any guests checking out today.
             </p>
@@ -215,6 +255,42 @@ const Hosting = () => {
               You don't have any guest reviews to write.
             </p>
           )}
+            </div>
+          )}
+          <div className="mx-4">
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={30}
+            slidesPerView={4}
+            navigation
+          >
+            {hostInfo.reservation[activeTab].map((info) => (
+              <SwiperSlide>
+                <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-xl">
+                  <div className="px-5 py-2">
+                    <h6 className="mb-2 text-red-600 text-xl font-semibold">
+                      {info.name}
+                    </h6>
+                    <h6 className="text-red-600 text-l font-light">
+                      {info.location}
+                    </h6>
+                    <p className="text-slate-600 leading-normal font-light">
+                      {activeTab === "checkingOut" ? `Check out by ${info.checkOutTime} on ${info.checkOutDate}` : `Check in at ${info.checkInTime} on ${info.checkInDate}`}
+                    </p>
+                    <div className="px-5 pb-4">
+                      <button
+                        className="text-black-500 underline hover:text-black-900 focus:outline-none disabled:text-black-700"
+                        type="button"
+                      >
+                        View reservation
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          </div>
         </div>
       </div>
     </div>
