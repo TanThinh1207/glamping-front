215ef8af945da244762640ba29f4de8799de1959
bmdjtt <thinhndtse182967@fpt.edu.vn>
2025-02-04 12:39:52 +0700

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -38,7 +38,7 @@ const NavigationBar = () => {
     const menuItems = isHostingPage
         ? [
             { name: "Today", link: "/hosting" },
-            { name: "Calendar", link: "/host/calendar" },
+            { name: "Calendar", link: "/hosting/calendar" },
             { name: "Listings", link: "/listings" },
         ]
         : [
